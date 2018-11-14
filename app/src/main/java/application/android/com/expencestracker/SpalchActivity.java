fc42560a925e4ba7217ddbb59c3317afe27cfef2
Seif-abedsslem <abedsslem.seif@yahoo.fr>
2018-11-14 16:21:31 +0100

    Initial commit

---
+++ app/src/main/java/application/android/com/expencestracker/SpalchActivity.java
@@ -0,0 +1,49 @@
+package application.android.com.expencestracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SpalchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalch);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageView.startAnimation(animation);
+
+
+        Thread timer = new Thread(){
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+
+        timer.start();
+    }
+}
