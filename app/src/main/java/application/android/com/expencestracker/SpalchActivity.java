0cc8db1a13ac7f7af5afd247b9545c89c303e7e6
Seif-abedsslem <abedsslem.seif@yahoo.fr>
2018-11-14 16:46:43 +0100

    Adding Splash Screen with animation Providing Access from Splash Screen to Login Screen Adding Icon Launcher for the application

--- app/src/main/java/application/android/com/expencestracker/SpalchActivity.java
+++ app/src/main/java/application/android/com/expencestracker/SpalchActivity.java
@@ -2,13 +2,7 @@ package application.android.com.expencestracker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
