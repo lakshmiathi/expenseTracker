294d969b277bb8549a4d3feb5b0a95a0c79a6985
madhulikavecha <madhu.anu5513@gmail.com>
2018-11-21 11:37:04 +0100

    About us page is done

--- app/src/main/java/application/android/com/expencestracker/AboutUsFragment.java
+++ app/src/main/java/application/android/com/expencestracker/AboutUsFragment.java
@@ -1,17 +1,28 @@
 package application.android.com.expencestracker;
 
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutUsFragment extends Fragment {
+
+
 
 
     public AboutUsFragment() {
@@ -22,8 +33,87 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        ExpandableListView expandableListView =(ExpandableListView)view.findViewById(R.id.Explistview);
+        expandableListView.setAdapter(new ExpandabelListAdoptor());
+
+        return view;
     }
 
+
+    public class ExpandabelListAdoptor extends BaseExpandableListAdapter {
+
+        private String[] aboutUs={"Introduction","Value Proposition","Contact Us"};
+        private String[][] child={
+                {"ExTrack is one stop Solution for all your expense management needs." +
+                        "ExTrack aims to provide a hassle free and enjoyable" +
+                        "expense management experience for all users across world." +
+                        "ExTrack is making a conscious effort to bring the power of Expense Management" +
+                        "to users and make track of all expenses in day to day life"
+                   },
+                {"ExTrack's value proposition revolves around giving customer the power and easy of tracking expenses Offline" +
+                        ".To make tracking easier for you,a dedicated customer connect team is on standby " +
+                        "to answer your queries .The next versions of the ExTrack is comming soon with lot of" +
+                        " new exciting features"},
+                {"For any queries or issues please feel free to contact us at ExtrackApp@gmail.com" +
+                        "Telephone: 0801255555"}
+        };
+
+        @Override
+        public int getGroupCount() {
+            return aboutUs.length;
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return child[groupPosition].length;
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return aboutUs[groupPosition];
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return child[groupPosition][childPosition];
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            TextView textView= new TextView(AboutUsFragment.this.getActivity());
+            textView.setText(getGroup(groupPosition).toString());
+            textView.setTypeface(null,Typeface.BOLD_ITALIC);
+            textView.setTextColor(Color.BLACK);
+            return textView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+           TextView textView = new TextView(AboutUsFragment.this.getActivity());
+           textView.setText(getChild(groupPosition,childPosition).toString());
+            return textView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
+    }
 }
