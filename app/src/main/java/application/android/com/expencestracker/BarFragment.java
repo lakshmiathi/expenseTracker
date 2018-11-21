55291df18e8b6b17a92ffa7d26eff79b7fe66a75
Seif-abedsslem <abedsslem.seif@yahoo.fr>
2018-11-21 15:20:47 +0100

    Seif and Daniel Work Implementation of the Pie Chart Implementation of the bar chart

--- app/src/main/java/application/android/com/expencestracker/BarFragment.java
+++ app/src/main/java/application/android/com/expencestracker/BarFragment.java
@@ -7,11 +7,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BarFragment extends Fragment {
+
+    BarChart chart ;
+    ArrayList<BarEntry> BARENTRY ;
+    ArrayList<String> BarEntryLabels ;
+    BarDataSet Bardataset ;
+    BarData BARDATA ;
+
 
 
     public BarFragment() {
@@ -19,11 +34,59 @@ public class BarFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_bar, container, false);
+        chart = (BarChart) view.findViewById(R.id.chart);
+
+
+
+
+        BARENTRY = new ArrayList<>();
+
+        BarEntryLabels = new ArrayList<String>();
+
+        AddValuesToBARENTRY();
+
+        AddValuesToBarEntryLabels();
+
+        Bardataset = new BarDataSet(BARENTRY, "Projects");
+
+        BARDATA = new BarData( BarEntryLabels, Bardataset);
+
+        Bardataset.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        chart.setData(BARDATA);
+
+        chart.animateY(3000);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bar, container, false);
+
+        return view;
+
+    }
+    public void AddValuesToBARENTRY(){
+
+        BARENTRY.add(new BarEntry(2f, 0));
+        BARENTRY.add(new BarEntry(15f, 1));
+        BARENTRY.add(new BarEntry(6f, 2));
+        BARENTRY.add(new BarEntry(8f, 3));
+        BARENTRY.add(new BarEntry(8f, 4));
+
+
+    }
+
+    public void AddValuesToBarEntryLabels(){
+
+        BarEntryLabels.add("Rent");
+        BarEntryLabels.add("Gorcery");
+        BarEntryLabels.add("Shopping");
+        BarEntryLabels.add("Travel");
+        BarEntryLabels.add("Others");
+
+
     }
 
 }
