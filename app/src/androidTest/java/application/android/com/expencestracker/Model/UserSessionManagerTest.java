4b4540e325751f32ae5ba8d0e36b7dec4b839e9e
Wei Wang <vivi666.wang@gmail.com>
2018-11-28 21:47:44 +0100

    finished unit test of class Expense

---
+++ app/src/androidTest/java/application/android/com/expencestracker/Model/UserSessionManagerTest.java
@@ -0,0 +1,39 @@
+package application.android.com.expencestracker.Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserSessionManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void createUserLoginSession() {
+
+
+    }
+
+    
+    @Test
+    public void checkLogIn() {
+
+
+    }
+
+    @Test
+    public void getUserDetails() {
+    }
+
+    @Test
+    public void logoutUser() {
+    }
+}
